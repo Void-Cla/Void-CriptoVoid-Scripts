20d19ff1d7b56a999f846d540bff2e3f54a142f1
voidkiller1 <voidcla.mg7@gmail.com>
2024-06-27 14:29:01 -0300

    d

--- TabelaMultBTC.js
+++ TabelaMultBTC.js
@@ -1,189 +1,181 @@
 // ==UserScript==
-// @name          Multiply BTC Tabela
-// @namespace     https://void-cla.github.io/criptovoid/
-// @version       1.0
-// @description   Este script monitora e exibe estatísticas detalhadas das rolagens no jogo Multiply BTC do Freebitco.in.
-// @author        Sr.fox
-// @match         https://freebitco.in/*
-// @grant         none
+// @name         Tabela de Rolagem do Multiply BTC
+// @namespace    
+// @version      0.1
+// @description  Script for freebitco.in that tracks and presents statistics on rolls in the Multiply BTC game, displaying them in a convenient table format.
+// @author       Sr.fox
+// @match        https://freebitco.in/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=freebitco.in
+// @grant        none
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    console.log("Status: Multiply BTC Tabela");
+    console.log("Status: Multiply BTC Roll Counter Script loaded");
 
-    // Contador global de rolagens
     let numRolls = 0;
-    // Elemento HTML para exibir o número de rolagens
     let numRollsTd = document.createElement('td');
 
-    // Estrutura de estatísticas por intervalo de odds
     const stat = {
-        4: { 'hi': 7625, 'lo': 2375, 'miss_hi': 0, 'miss_lo': 0 },
-        8: { 'hi': 8813, 'lo': 1188, 'miss_hi': 0, 'miss_lo': 0 },
-        19: { 'hi': 9500, 'lo': 500, 'miss_hi': 0, 'miss_lo': 0 },
-        34: { 'hi': 9721, 'lo': 279, 'miss_hi': 0, 'miss_lo': 0 },
-        64: { 'hi': 9852, 'lo': 148, 'miss_hi': 0, 'miss_lo': 0 },
-        128: { 'hi': 9926, 'lo': 74, 'miss_hi': 0, 'miss_lo': 0 },
-        256: { 'hi': 9963, 'lo': 37, 'miss_hi': 0, 'miss_lo': 0 },
-        512: { 'hi': 9981, 'lo': 19, 'miss_hi': 0, 'miss_lo': 0 },
-        1250: { 'hi': 9992, 'lo': 8, 'miss_hi': 0, 'miss_lo': 0 },
-        2000: { 'hi': 9995, 'lo': 5, 'miss_hi': 0, 'miss_lo': 0 },
-        4750: { 'hi': 9998, 'lo': 2, 'miss_hi': 0, 'miss_lo': 0 },
-        8888: { 'hi': 0, 'lo': 0, 'miss_hi': 0, 'miss_lo': 0 }
+        1.25: {
+            'hi': 8000,
+            'lo': 2000,
+            'miss_hi': 0,
+            'miss_lo': 0,
+            'miss_hi_td': document.createElement('td'),
+            'miss_lo_td': document.createElement('td'),
+        },
+        2.0: {
+            'hi': 9000,
+            'lo': 1000,
+            'miss_hi': 0,
+            'miss_lo': 0,
+            'miss_hi_td': document.createElement('td'),
+            'miss_lo_td': document.createElement('td'),
+        },
+        4: {
+            'hi': 7625,
+            'lo': 2375,
+            'miss_hi': 0,
+            'miss_lo': 0,
+            'miss_hi_td': document.createElement('td'),
+            'miss_lo_td': document.createElement('td'),
+        },
+        8: {
+            'hi': 8813,
+            'lo': 1188,
+            'miss_hi': 0,
+            'miss_lo': 0,
+            'miss_hi_td': document.createElement('td'),
+            'miss_lo_td': document.createElement('td'),
+        },
+        19: {
+            'hi': 9500,
+            'lo': 500,
+            'miss_hi': 0,
+            'miss_lo': 0,
+            'miss_hi_td': document.createElement('td'),
+            'miss_lo_td': document.createElement('td')
+        },
+        34: {
+            'hi': 9721,
+            'lo': 279,
+            'miss_hi': 0,
+            'miss_lo': 0,
+            'miss_hi_td': document.createElement('td'),
+            'miss_lo_td': document.createElement('td')
+        },
+        64: {
+            'hi': 9852,
+            'lo': 148,
+            'miss_hi': 0,
+            'miss_lo': 0,
+            'miss_hi_td': document.createElement('td'),
+            'miss_lo_td': document.createElement('td')
+        },
+        128: {
+            'hi': 9926,
+            'lo': 74,
+            'miss_hi': 0,
+            'miss_lo': 0,
+            'miss_hi_td': document.createElement('td'),
+            'miss_lo_td': document.createElement('td')
+        },
+        256: {
+            'hi': 9963,
+            'lo': 37,
+            'miss_hi': 0,
+            'miss_lo': 0,
+            'miss_hi_td': document.createElement('td'),
+            'miss_lo_td': document.createElement('td')
+        },
+        512: {
+            'hi': 9981,
+            'lo': 19,
+            'miss_hi': 0,
+            'miss_lo': 0,
+            'miss_hi_td': document.createElement('td'),
+            'miss_lo_td': document.createElement('td')
+        },
+        4750: {
+            'hi': 9998,
+            'lo': 2,
+            'miss_hi': 0,
+            'miss_lo': 0,
+            'miss_hi_td': document.createElement('td'),
+            'miss_lo_td': document.createElement('td')
+        },
+        8888: {
+            'hi': 0,
+            'lo': 0,
+            'miss_hi': 0,
+            'miss_lo': 0,
+            'miss_hi_td': document.createElement('td'),
+            'miss_lo_td': document.createElement('td')
+        }
     };
 
-    // Criação do elemento de interface fixa na página
     let div = document.createElement('div');
-    div.style.cssText = 'position:fixed;top:50px;left:0;background-color:white;padding:10px;border:1px solid #ccc;z-index:9999;';
+    div.style.cssText = 'position:fixed;top:50px;left:0;background-color:white;';
 
-    // Função para criar as linhas de cabeçalho na tabela
-    function tableHeaderRows() {
-        // Linha do título
-        let titleTr = document.createElement('tr');
-        let titleTh = document.createElement('th');
-        titleTh.colSpan = 6; // Número total de colunas na tabela
-        titleTh.style.cssText = 'text-align: center;'; // Centraliza o texto
-        titleTh.innerHTML = '<strong>Multiply BTC Tabela</strong>';
-        titleTr.appendChild(titleTh);
-
-        // Linha de cabeçalhos
-        let headerTr = document.createElement('tr');
-        headerTr.innerHTML = '<th>Odds</th><th>HI</th><th>LO</th><th>HI</th><th>LO</th>';
-
-        return [titleTr, headerTr];
-    }
-
-    // Função para criar a linha de rolagens na tabela
-    function tableRollsRow() {
-        let tr = document.createElement('tr');
-        let tdLabel = document.createElement('td');
-        let boldLabel = document.createElement('strong');
-        boldLabel.textContent = 'Rolagens';
-        tdLabel.appendChild(boldLabel);
-
-        // Células vazias para alinhamento
-        tr.appendChild(tdLabel);
-        tr.appendChild(document.createElement('td'));
-        tr.appendChild(numRollsTd);
-        return tr;
-    }
-
-    // Função para criar a linha de resultado provável na tabela
-    function tableProbableResultRow() {
-        let tr = document.createElement('tr');
-        tr.id = 'probable-result-row'; // Atribui um ID para fácil acesso
-
-        let tdLabel = document.createElement('td');
-        tdLabel.colSpan = 6; // Número total de colunas na tabela
-        tdLabel.style.cssText = 'text-align: center;'; // Centraliza o texto
-        tdLabel.innerHTML = '<strong>Provavelmente será: </strong><span id="probable-result">N/A</span>';
-
-        tr.appendChild(tdLabel);
-        return tr;
-    }
-
-    // Criação da tabela para exibir estatísticas
     let table = document.createElement('table');
 
-    // Obtenção das linhas de cabeçalho
-    let headerRows = tableHeaderRows();
-    table.appendChild(headerRows[0]); // Adiciona a linha do título
-    table.appendChild(headerRows[1]); // Adiciona a linha de cabeçalhos
+    let rollsTr = tableRollsRow();
+    let headerTr = tableHeaderRow();
+    table.appendChild(rollsTr);
+    table.appendChild(headerTr);
 
-    // Adiciona linhas para cada intervalo de odds com suas estatísticas
     for (let betLabel in stat) {
         let tr = document.createElement('tr');
 
         let tdLabel = document.createElement('td');
-        tdLabel.textContent = betLabel;
+        tdLabel.innerHTML = '<span>'+ betLabel +'</span>';
 
-        let tdHi = document.createElement('td');
-        tdHi.textContent = stat[betLabel]['hi'];
-
-        let tdLo = document.createElement('td');
-        tdLo.textContent = stat[betLabel]['lo'];
-
-        let tdMissHi = document.createElement('td');
-        tdMissHi.textContent = stat[betLabel]['miss_hi'];
-
-        let tdMissLo = document.createElement('td');
-        tdMissLo.textContent = stat[betLabel]['miss_lo'];
-
-        // Atribui IDs para cada célula de estatística para atualização dinâmica
-        tdMissHi.id = 'stat-' + betLabel + '-miss-hi';
-        tdMissLo.id = 'stat-' + betLabel + '-miss-lo';
+        let tdHi = stat[betLabel]['miss_hi_td'];
+        let tdLo = stat[betLabel]['miss_lo_td'];
 
         tr.appendChild(tdLabel);
         tr.appendChild(tdHi);
         tr.appendChild(tdLo);
-        tr.appendChild(tdMissHi);
-        tr.appendChild(tdMissLo);
 
         table.appendChild(tr);
     }
 
-    // Adiciona a linha de resultado provável
-    table.appendChild(tableProbableResultRow());
-
-    // Adiciona a tabela ao elemento de interface
     div.appendChild(table);
-    // Adiciona o elemento de interface ao documento
     document.documentElement.appendChild(div);
 
-    // Função para calcular o resultado mais provável
-    function calculateProbableResult() {
-        let probableResult = 'N/A'; // Valor padrão
-        let minMissHi = Infinity;
-        let minMissLo = Infinity;
-
-        for (let betLabel in stat) {
-            if (stat[betLabel]['miss_hi'] < minMissHi) {
-                minMissHi = stat[betLabel]['miss_hi'];
-                probableResult = `HI ${betLabel}`;
-            }
-            if (stat[betLabel]['miss_lo'] < minMissLo) {
-                minMissLo = stat[betLabel]['miss_lo'];
-                probableResult = `LO ${betLabel}`;
-            }
-        }
-
-        return probableResult;
-    }
-
-    // Função para redesenhar as estatísticas na tabela
     function redraw() {
-        numRollsTd.textContent = numRolls;
+        numRollsTd.innerHTML = ''+numRolls+'';
 
         for (let betLabel in stat) {
-            document.getElementById('stat-' + betLabel + '-miss-hi').textContent = stat[betLabel]['miss_hi'];
-            document.getElementById('stat-' + betLabel + '-miss-lo').textContent = stat[betLabel]['miss_lo'];
+            stat[betLabel]['miss_hi_td'].innerHTML = ''+stat[betLabel]['miss_hi']+'';
+            stat[betLabel]['miss_lo_td'].innerHTML = ''+stat[betLabel]['miss_lo']+'';
         }
 
-        // Atualiza o resultado mais provável
-        document.getElementById('probable-result').textContent = calculateProbableResult();
+        // Chamada da função para decidir o próximo roll e exibir no console
+        console.log('Próximo roll provavelmente será:', decideNextRoll());
     }
 
-    // Obtém o elemento onde a tabela de histórico de apostas é atualizada
     let table_rows = document.getElementById('bet_history_table_rows');
-
-    // Cria um array para armazenar todas as rolagens
     let rolls = [];
 
-    // Cria um observador de mutação para monitorar a tabela de histórico de apostas
     let observer = new MutationObserver(function(mutationsList) {
+        let roll;
         for (let mutation of mutationsList) {
             for (let addedNode of mutation.addedNodes) {
                 if (addedNode.className == 'multiply_bet_history_table_row') {
-                    // Obtém a rolagem adicionada
-                    let roll = parseInt(addedNode.firstChild.childNodes[3].textContent);
+                    roll = parseInt(addedNode.firstChild.childNodes[3].textContent);
 
-                    // Verifica se a rolagem está dentro do intervalo válido
-                    if (!isNaN(roll) && roll >= 0 && roll <= 10000) {
+                    if (isNaN(roll)) {
+                        console.error('roll is Not a number');
+                        continue;
+                    }
+
+                    if (roll >= 0 && roll <= 10000) {
                         numRolls++;
 
-                        // Atualiza as estatísticas para cada intervalo de odds
                         for (let betLabel in stat) {
                             if (betLabel == 8888) {
                                 if (roll == 8888) {
@@ -196,7 +188,6 @@
                                 continue;
                             }
 
-                            // Atualiza miss_hi e miss_lo com base na rolagem
                             if (roll > stat[betLabel]['hi']) {
                                 stat[betLabel]['miss_hi'] = 0;
                             } else {
@@ -209,22 +200,87 @@
                                 stat[betLabel]['miss_lo']++;
                             }
                         }
+
+                        // Redesenha a tabela e decide o próximo roll após cada roll observado
+                        redraw();
                     } else {
-                        console.error('Rolagem inválida:', roll);
+                        console.error('roll is Not in valid range');
                     }
                 }
             }
         }
-
-        // Redesenha a tabela de estatísticas após a atualização
-        redraw();
     });
 
-    // Configuração do observador para observar mudanças na tabela de histórico de apostas
     let config = {
-        childList: true
+        childList: true,
     };
 
-    // Inicia a observação
     observer.observe(table_rows, config);
+
+    function tableRollsRow() {
+        let fragment = document.createDocumentFragment();
+
+        let tr = document.createElement('tr');
+        let td = document.createElement('td');
+        let bold = document.createElement('strong');
+
+        let rollsLabel = document.createTextNode('Rolls');
+
+        bold.appendChild(rollsLabel);
+        td.appendChild(bold);
+
+        tr.appendChild(td);
+        tr.appendChild(td.cloneNode());
+        tr.appendChild(numRollsTd);
+
+        fragment.appendChild(tr);
+        return fragment;
+    }
+
+    function tableHeaderRow() {
+        let fragment = document.createDocumentFragment();
+
+        let tr = document.createElement('tr');
+
+        let label1 = document.createTextNode('Odds');
+        let label2 = document.createTextNode('HI');
+        let label3 = document.createTextNode('LO');
+
+        let bLabel1 = document.createElement('strong');
+        bLabel1.appendChild(label1);
+
+        let bLabel2 = document.createElement('strong');
+        bLabel2.appendChild(label2);
+
+        let bLabel3 = document.createElement('strong');
+        bLabel3.appendChild(label3);
+
+        let td1 = document.createElement('td');
+        td1.appendChild(bLabel1);
+
+        let td2 = document.createElement('td');
+        td2.appendChild(bLabel2);
+
+        let td3 = document.createElement('td');
+        td3.appendChild(bLabel3);
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        fragment.appendChild(tr);
+        return fragment;
+    }
+
+    // Função para determinar se o próximo roll tem mais chance de ser HI ou LO
+    function decideNextRoll() {
+        // Exemplo simples: se o valor atual de miss_hi > miss_lo para uma certa odd, retorna 'HI', senão 'LO'
+        let odd = 1.25; // A odd que você quer consultar
+        if (stat[odd]['miss_hi'] > stat[odd]['miss_lo']) {
+            return 'HI';
+        } else {
+            return 'LO';
+        }
+    }
 })();
+
